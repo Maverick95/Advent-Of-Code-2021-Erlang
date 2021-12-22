d3fe2484806cd20f06d49de6772af5bb0a19126f
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-12-22 00:33:03 +0000

    Separate query and add functionality for main

--- Day 2 OTP/main.erl
+++ Day 2 OTP/main.erl
@@ -2,6 +2,7 @@
 -export([
     start/0,
     add/1,
+    query/0,
     stop/0
 ]).
 
@@ -9,10 +10,21 @@ start() ->
     gen_event:start_link({local, aoc}),
     gen_event:add_handler(aoc, {data_handler, part1}, {part1, part1, part1_logger}),
     gen_event:add_handler(aoc, {data_handler, part2}, {part2, part2, part2_logger}),
+    gen_server:start_link({local, aoc_input_transform}, input_transform, [], []),
     ok.
 
 add(Data) ->
-    gen_event:notify(aoc, Data).
+    Event = gen_server:call(aoc_input_transform, Data),
+    case Event of
+        error ->
+            error;
+        _ ->
+            gen_event:notify(aoc, Event)
+    end.
+
+query() ->
+    gen_event:notify(aoc, result).
     
 stop() ->
-    gen_event:stop(aoc).
+    gen_event:stop(aoc),
+    gen_server:stop(aoc_input_transform).
