6381bda84e8a2d523f972c176a20d37d3604088e
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-12-25 15:35:44 +0000

    Switch to using supervisor with no halt functionality

--- Common/main.erl
+++ Common/main.erl
@@ -1,38 +1,14 @@
 -module(main).
--export([
-    start/2,
-    terminal/1,
-    file/1,
-    query/0,
-    stop/0
-]).
+-export([start/2, terminal/1, file/1,query/0]).
 
 start(Server, Transform) ->
-
-    gen_event:start_link({local, aoc_logger_manager}),
-    gen_event:add_handler(aoc_logger_manager, log_handler, aoc_logger),
-
-    gen_event:start_link({local, aoc_manager}),    
-    gen_event:add_handler(aoc_manager, {data_handler, handler}, {Server, aoc_logger_manager}),
-
-    gen_server:start_link({local, aoc_input_transform}, Transform, [], []),
-
-    gen_event:start_link({local, aoc_input_manager}),
-    gen_event:add_handler(aoc_input_manager, input_handler, {aoc_input_transform, aoc_manager}),
-
-    ok.
+    supervisor:start_link({local, main_supervisor}, main_supervisor, {Server, Transform}).
 
 terminal(Data) ->
-    spawn(import, terminal, [Data, aoc_input_manager]).
+    spawn(main_supervisor, start_input_terminal, [Data]).
 
 file(File) ->
-    spawn(import, file, [File, aoc_input_manager]).
+    spawn(main_supervisor, start_input_file, [File]).
 
 query() ->
-    gen_event:notify(aoc_manager, result).
-    
-stop() -> 
-    gen_event:stop(aoc_input_manager),
-    gen_server:stop(aoc_input_transform),
-    gen_event:stop(aoc_manager),
-    gen_event:stop(aoc_logger_manager).
+    spawn(main_supervisor, start_query, []).
---
+++ Common/main_supervisor.erl
@@ -0,0 +1,75 @@
+-module(main_supervisor).
+-behaviour(supervisor).
+-export([
+    start_logger/0,
+    start_data_handler/1,
+    start_input_handler/0,
+    start_input_terminal/1,
+    start_input_file/1,
+    start_query/0,
+    init/1
+]).
+
+
+
+start_logger() ->
+    Handler = gen_event:start_link({local, aoc_logger_manager}),
+    gen_event:add_handler(aoc_logger_manager, log_handler, aoc_logger),
+    Handler.
+
+
+
+start_data_handler(Server) ->
+    Handler = gen_event:start_link({local, aoc_manager}),    
+    gen_event:add_handler(aoc_manager, data_handler, {Server, aoc_logger_manager}),
+    Handler.
+
+
+
+start_input_handler() ->
+    Handler = gen_event:start_link({local, aoc_input_manager}),
+    gen_event:add_handler(aoc_input_manager, input_handler, {aoc_input_transform, aoc_manager}),
+    Handler.
+
+
+
+start_input_terminal(Data) ->
+    import:terminal(Data, aoc_input_manager).
+
+
+
+start_input_file(File) ->
+    import:file(File, aoc_input_manager).
+
+
+
+start_query() ->
+    gen_event:notify(aoc_manager, result).
+
+
+
+init({Server, Transform}) ->
+    {
+        ok,
+        {
+            #{},
+            [
+                #{
+                    id => sup_logger,
+                    start => {main_supervisor, start_logger, []}
+                },
+                #{
+                    id => sup_data_handler,
+                    start => {main_supervisor, start_data_handler, [Server]}
+                },
+                #{
+                    id => sup_input_server,
+                    start => {gen_server, start_link, [{local, aoc_input_transform}, Transform, [], []]}
+                },
+                #{
+                    id => sup_input_handler,
+                    start => {main_supervisor, start_input_handler, []}
+                }
+            ]
+        }
+    }.
