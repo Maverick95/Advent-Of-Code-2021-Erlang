193f1feed39fd2443c1691afb0294f8841070d4a
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-12-25 01:05:29 +0000

    Re-order main to stop in reverse order to start

--- Common/main.erl
+++ Common/main.erl
@@ -18,7 +18,6 @@ start(Server, Transform) ->
     gen_server:start_link({local, aoc_input_transform}, Transform, [], []),
 
     gen_event:start_link({local, aoc_input_manager}),
-
     gen_event:add_handler(aoc_input_manager, input_handler, {aoc_input_transform, aoc_manager}),
 
     ok.
@@ -32,8 +31,8 @@ file(File) ->
 query() ->
     gen_event:notify(aoc_manager, result).
     
-stop() ->
-    gen_event:stop(aoc_manager),
+stop() -> 
     gen_event:stop(aoc_input_manager),
     gen_server:stop(aoc_input_transform),
+    gen_event:stop(aoc_manager),
     gen_event:stop(aoc_logger_manager).
