f70721c594601c9747649efc6cb2648b9272f70f
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2022-01-01 20:45:01 +0000

    First steps towards writing function to replace server code

--- advent_of_code/src/common/advent_of_code.erl
+++ advent_of_code/src/common/advent_of_code.erl
@@ -6,6 +6,7 @@
     file/1,
     process/1,
     query/0,
+    reload/1,
     stop/1
 ]).
 
@@ -26,3 +27,63 @@ query() ->
 
 stop(_) ->
     ok.
+
+
+
+
+
+reload(Day) ->
+
+    % Compile the 3 required code files into ebin.
+    AsciiDay = day_to_ascii(Day),
+    Directory = "../src/day" ++ AsciiDay,
+    Files = ["part1", "part2", "input_transform"],
+    lists:map(
+        fun(File) ->
+            FileAtom = list_to_atom(File),
+            CompileResult = compile:file(Directory ++ "/" ++ File),
+            case CompileResult of
+                error -> error;
+                _ ->
+                    Loaded =
+                    case code:is_loaded(FileAtom) of
+                        {file, _} -> true;
+                        _ -> false
+                    end,
+                    Deleted = code:delete(FileAtom),
+                    Purge = Loaded and (not Deleted),
+                    load(FileAtom, Purge)
+            end
+        end, Files).
+
+
+
+
+day_to_ascii(Day) when Day > 0 ->
+    day_to_ascii(Day, []).
+
+day_to_ascii(Day, Current) when Day == 0 ->
+    Current;
+
+day_to_ascii(Day, Current) ->
+    Remainder = Day rem 10,
+    Head = 48 + Remainder,
+    Next = (Day - Remainder) div 10,
+    day_to_ascii(Next, [Head | Current]).
+
+
+
+load(FileAtom, false) ->
+    straight_load(FileAtom);
+
+load(FileAtom, true) ->
+    code:purge(FileAtom),
+    code:delete(FileAtom),
+    straight_load(FileAtom).
+
+
+
+straight_load(FileAtom) ->
+    {Result, _} = code:load_file(FileAtom),
+    Result.
+
